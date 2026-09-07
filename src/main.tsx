6ae9778724959b1de92e25a3755ff05d4d060306
KRISHNA BHARGAVA A <75658985+KrishnaBhargavaAnantharamaiah@users.noreply.github.com>
2026-09-07 08:46:59 +0530

    Load employee protection section styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './logo-overrides.css'
+import './employee-protection.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
